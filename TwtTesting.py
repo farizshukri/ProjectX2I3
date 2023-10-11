4ea66ad323eb6f9718b71cc0989e42f110957edc
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2023-10-11 18:20:30 +0800

    initial commit

---
+++ TwtTesting.py
@@ -0,0 +1,18 @@
+import tweepy
+
+# Replace with your own keys and tokens
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Tweet a message
+tweet_message = "Hello, this is my first tweet using a Twitter bot!"
+api.update_status(tweet_message)
