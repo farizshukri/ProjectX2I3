dd959d79f9dddd2919b8d938a656d71f11c2e292
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2023-11-02 18:20:30 +0800

    initial commit

---
+++ AudioReader.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
+import pyttsx3
+
+def read_file(file_path):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed percent (can go over 100)
+    engine.setProperty('volume', 1)  # Volume 0-1
+    with open(file_path, 'r') as file:
+        text = file.read()
+        engine.say(text)
+        engine.runAndWait()
+
+def select_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path:
+        read_file(file_path)
+
+def main():
+    root = tk.Tk()
+    root.title("Text-to-Speech Reader")
+
+    label = tk.Label(root, text="Select a text file to read aloud:")
+    label.pack(pady=10)
+
+    button = tk.Button(root, text="Select File", command=select_file)
+    button.pack(pady=10)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
